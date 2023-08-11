d018090c2d529df5026285ea3758ab4a550d9cdf
PRoy <sam469roger@gmail.com>
2023-08-11 23:31:04 +0530

    implementing sticky navigation bar

--- js/script.js
+++ js/script.js
@@ -41,6 +41,28 @@ allLinks.forEach(function (link) {
 });
 
 ///////////////////////////////////////////////////////////
+// sticky navigation
+const sectionHeroEl = document.querySelector('.section-hero');
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (!ent.isIntersecting) {
+      document.body.classList.add('sticky');
+    } else {
+      document.body.classList.remove('sticky');
+    }
+  },
+  {
+    // in the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: '-80px',
+  }
+);
+obs.observe(sectionHeroEl);
+
+///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
   var flex = document.createElement('div');
